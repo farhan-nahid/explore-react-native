4b65e434d11182fefa69793de5628021deeeec32
gesora96 <nahidahmed9933@gmail.com>
2022-05-13 22:57:47 +0600

    input added

--- App.js
+++ App.js
@@ -1,23 +1,37 @@
 import { StatusBar } from 'expo-status-bar';
 import React, { useState } from 'react';
-import { Button, StyleSheet, Text, View } from 'react-native';
+import { Button, StyleSheet, Text, TextInput, View } from 'react-native';
 
 export default function App() {
   const [name, setName] = useState(false);
+  const [inputName, setInputName] = useState(false);
 
   return (
     <View style={styles.container}>
       <View>
         <Text style={styles.heading}>Hello World !</Text>
       </View>
-      <View style={styles.body}>
+      <View style={styles.mt10}>
         <Text>
-          My Name is <Text style={styles.myName}>{name ? 'Farhan' : 'Nahid'}</Text>{' '}
+          My Name is <Text style={styles.myName}>{name ? 'Farhan' : 'Nahid'}</Text>
         </Text>
       </View>
 
       <View style={styles.mainButton}>
         <Button title='Update Name' onPress={() => setName((prevState) => !prevState)} />
+      </View>
+
+      <View>
+        <TextInput
+          style={styles.input}
+          placeholder='John Cena'
+          placeholderTextColor='#A0BCC2'
+          onChangeText={(value) => setInputName(value)}
+        />
+        <Text style={styles.mt10}>
+          Input Name:{' '}
+          <Text style={styles.myName}>{inputName ? inputName : 'John Cena'}</Text>
+        </Text>
       </View>
       <StatusBar style='auto' />
     </View>
@@ -40,7 +54,7 @@ const styles = StyleSheet.create({
     borderRadius: 10,
     fontSize: 16,
   },
-  body: {
+  mt10: {
     marginTop: 20,
   },
   myName: {
@@ -50,5 +64,16 @@ const styles = StyleSheet.create({
   },
   mainButton: {
     marginTop: 20,
+  },
+  input: {
+    marginTop: 30,
+    borderWidth: 1,
+    borderColor: 'teal',
+    paddingLeft: 20,
+    paddingRight: 20,
+    paddingTop: 5,
+    paddingBottom: 5,
+    width: 200,
+    borderRadius: 10,
   },
 });
